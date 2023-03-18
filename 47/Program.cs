7befc15d2279960b86258aa4324b77f1cbc051a4
Sergy <zudkov@ya.ru>
2023-03-18 18:54:10 +0300

    47

---
+++ 47/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"m = {m}, n = {n}.");
+double[,] array = new double[m, n];
+Arrayvvod(array);
+WriteArray(array);
+void Arrayvvod(double[,] array)
+{
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      array[i, j] = new Random().NextDouble() * 20 - 10;
+    }
+  }
+}
+void WriteArray (double[,] array){
+for (int i = 0; i < m; i++)
+  {
+      for (int j = 0; j < n; j++)
+      {
+        double Num = Math.Round(array[i, j], 1);
+        Console.Write(Num + " ");
+      }
+      }
+}
