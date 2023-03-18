769a5735e09b485984001d5b466669a88ff370b1
Sergy <zudkov@ya.ru>
2023-03-18 19:56:04 +0300

    52

---
+++ 52/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+
+int[,] Fullarr = new int[m, n];
+Fullarr = NewArr(array);
+
+WriteArray(Fullarr);
+
+Console.Write($"Cреднее арифметическое равно ");
+for (int i = 0; i < n; i++)
+{
+  double arithmeticMean = 0;
+  for (int j = 0; j < m; j++)
+  {
+    arithmeticMean += Fullarr[j, i];
+  }
+  arithmeticMean = Math.Round(arithmeticMean / m, 1);
+  Console.WriteLine($"столбца № {i+1} {arithmeticMean}");
+}
+
+int[,] NewArr (int[,] array)
+{
+  int[,] Fullarr = new int[array.GetLength(0), array.GetLength(1)];
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Fullarr[i, j] = Convert.ToInt32(array[i, j]);
+    }
+  }
+  return Fullarr;
+}
+
+void WriteArray (int[,] Fullarr)
+{
+for (int i = 0; i < m; i++)
+  {
+      for (int j = 0; j < n; j++)
+      {
+        Console.Write(Fullarr[i, j] + " ");
+      }
+      Console.WriteLine();
+  }
+}
